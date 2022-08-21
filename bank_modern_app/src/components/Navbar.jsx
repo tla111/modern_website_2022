2b7616db83beea32710badb620fdbc775af55207
Tim La <tlamarketing111@gmail.com>
2022-08-21 10:49:31 -0400

    populated nav links

--- bank_modern_app/src/components/Navbar.jsx
+++ bank_modern_app/src/components/Navbar.jsx
@@ -6,7 +6,18 @@ import { navLinks } from '../constants'
 
 const Navbar = () => {
     return (
-        <nav className="w-full flex py-6 justify-between items-center navbar">Navbar</nav>
+        <nav className="w-full flex py-6 justify-between items-center navbar">
+            <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
+            <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+                {navLinks.map((nav, index) => (
+                    <li key={nav.id} className={`font-poppins`}>
+                        <a href={`#${nav.id}`}>
+                            {nav.title}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </nav>
     )
 }
 
