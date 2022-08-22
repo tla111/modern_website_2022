c5c31b9a6183f1d41bb73842c61a36446c010480
Tim La <tlamarketing111@gmail.com>
2022-08-22 10:58:53 -0400

    copied description text

--- bank_modern_app/src/components/Hero.jsx
+++ bank_modern_app/src/components/Hero.jsx
@@ -28,6 +28,10 @@ const Hero = () => {
                 <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full">
                     Payment Method
                 </h1>
+                <p>  Our team of experts uses a methodology to identify the credit cards
+                    most likely to fit your needs. We examine annual percentage rates,
+                    annual fees.
+                </p>
 
             </div>
         </section>
