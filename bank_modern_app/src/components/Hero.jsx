f87178e7418d0493fabcddb06b09e518eeae9878
Tim La <tlamarketing111@gmail.com>
2022-08-22 10:33:12 -0400

    imported styles & assets

--- bank_modern_app/src/components/Hero.jsx
+++ bank_modern_app/src/components/Hero.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import styles from '../style';
+import { discount, robot } from '../assets';
 
 const Hero = () => {
     return (
-        <div>Hero</div>
+        <section id="home" className={``}>
+
+        </section>
     )
 }
 
