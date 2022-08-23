24d7a21b8bbb91340f2d476b34bb1307fb8cec6b
Tim La <tlamarketing111@gmail.com>
2022-08-23 10:56:09 -0400

    styled h2 & p

--- bank_modern_app/src/components/CardDeal.jsx
+++ bank_modern_app/src/components/CardDeal.jsx
@@ -6,8 +6,8 @@ import Button from './Button'
 const CardDeal = () => (
     <section className={layout.section}>
         <div className={layout.sectionInfo}>
-            <h2>Find a better card deal <br className="sm:block hidden" /> in a few easy steps</h2>
-            <p> Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis
+            <h2 className={styles.heading2}>Find a better card deal <br className="sm:block hidden" /> in a few easy steps</h2>
+            <p className={`${styles.paragraph} max-w-[470px] mt-5`}> Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis
                 aliquet eget mauris tortor.ç Aliquet ultrices ac, ametau.
             </p>
         </div>
