0507d1ec112d14788c27aa87d3ce480d6f938bff
Tim La <tlamarketing111@gmail.com>
2022-08-23 11:35:13 -0400

    added text & button for CTA component

--- bank_modern_app/src/components/CTA.jsx
+++ bank_modern_app/src/components/CTA.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import styles from '../style'
+import Button from './Button'
 
-const CTA = () => {
-    return (
-        <div>CTA</div>
-    )
-}
+
+const CTA = () => (
+    <section>
+        <div>
+            <h2>Let's try our service now!</h2>
+            <p>  Everything you need to accept card payments and grow your business
+                anywhere on the planet.
+            </p>
+        </div>
+
+        <div>
+            <Button />
+        </div>
+    </section>
+)
 
 export default CTA
