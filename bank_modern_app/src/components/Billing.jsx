afef08803bada8d6739de724ad594fde47a3d917
Tim La <tlamarketing111@gmail.com>
2022-08-23 07:50:38 -0400

    added apple & google button

--- bank_modern_app/src/components/Billing.jsx
+++ bank_modern_app/src/components/Billing.jsx
@@ -17,6 +17,10 @@ const Billing = () => (
                 aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea
                 placerat.
             </p>
+            <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
+                <img src={apple} alt="apple__store" className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer" />
+                <img src={google} alt="google__play" className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer" />
+            </div>
         </div>
     </section>
 )
