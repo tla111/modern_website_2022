6f4e3eed26ff48532972f232bfe89f49ca26ffd8
Tim La <tlamarketing111@gmail.com>
2022-08-23 11:52:53 -0400

    styled footerlinks titles

--- bank_modern_app/src/components/Footer.jsx
+++ bank_modern_app/src/components/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => (
             <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10">
                 {footerLinks.map((footerLink) => (
                     <div key={footerLink.key} className="flex flex-col ss:my-0 my-4 min-w-[150px]">
-                        <h4>{footerLink.title}</h4>
+                        <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-white">{footerLink.title}</h4>
                         <ul>
                             {footerLink.links.map((link, index) => (
                                 <li key={link.map} className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer`}>
