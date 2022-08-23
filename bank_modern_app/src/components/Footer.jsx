7aa598869050df6a93779ad32ec61e3c8286cf4d
Tim La <tlamarketing111@gmail.com>
2022-08-23 11:51:33 -0400

    mapped & populated links

--- bank_modern_app/src/components/Footer.jsx
+++ bank_modern_app/src/components/Footer.jsx
@@ -8,7 +8,24 @@ const Footer = () => (
         <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
             <div className="flex-1 flex flex-col justify-start mr-10">
                 <img src={logo} alt="hoobank" className="w-[266px] h-[72px] object-contain" />
+                <p className={`${styles.paragraph} mt-4 max-w-[310px]`}>A new way to make the payments easy, reliable and secure.</p>
             </div>
+
+            <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10">
+                {footerLinks.map((footerLink) => (
+                    <div key={footerLink.key} className="flex flex-col ss:my-0 my-4 min-w-[150px]">
+                        <h4>{footerLink.title}</h4>
+                        <ul>
+                            {footerLink.links.map((link, index) => (
+                                <li key={link.map} className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer`}>
+                                    {link.name}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                ))}
+            </div>
+
         </div>
     </section>
 )
