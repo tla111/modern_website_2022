2c36a60f9cbc1664fa1cc1fd0da96aff8580fd64
Tim La <tlamarketing111@gmail.com>
2022-08-23 07:18:44 -0400

    rendered & styled stats

--- bank_modern_app/src/components/Stats.jsx
+++ bank_modern_app/src/components/Stats.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { stats } from '../constants'
+import styles from '../style'
 
-const Stats = () => {
-    return (
-        <div>Stats</div>
-    )
-}
+const Stats = () => (
+    <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}>
+        {stats.map((stat) => (
+            <div key={stat.id} className="">
+                <h4 className="font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] leading-[40px] text-white">{stat.value}</h4>
+                <p>{stat.title}</p>
+            </div>
+        ))}
+    </section>
+)
 
 export default Stats
