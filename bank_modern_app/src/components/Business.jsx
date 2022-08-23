91875bc02849326fee86b23eefd8a0b79cfcdc74
Tim La <tlamarketing111@gmail.com>
2022-08-23 07:29:25 -0400

    created & styled button component

--- bank_modern_app/src/components/Business.jsx
+++ bank_modern_app/src/components/Business.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { features } from '../constants'
 import styles, { layout } from '../style'
-import button from './Button'
+import Button from './Button'
 
 const Business = () => {
     return (
@@ -10,10 +10,12 @@ const Business = () => {
                 <h2 className={styles.heading2}> You do the business, <br className="sm:block hidden" /> we’ll handle
                     the money.
                 </h2>
-                <p className={`${styles.paragraph} max-w-[470px]`}>With the right credit card, you can improve your financial life by
+                <p className={`${styles.paragraph} max-w-[470px] mt-5`}>With the right credit card, you can improve your financial life by
                     building credit, earning rewards and saving money. But with hundreds
                     of credit cards on the market.
                 </p>
+
+                <Button styles="mt-10" />
             </div>
         </section>
     )
