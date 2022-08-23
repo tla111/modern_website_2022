8195d78e8e3df10a48f0ae857afadc7f6646a8fa
Tim La <tlamarketing111@gmail.com>
2022-08-23 11:55:00 -0400

    styled footer component

--- bank_modern_app/src/components/Footer.jsx
+++ bank_modern_app/src/components/Footer.jsx
@@ -15,9 +15,9 @@ const Footer = () => (
                 {footerLinks.map((footerLink) => (
                     <div key={footerLink.key} className="flex flex-col ss:my-0 my-4 min-w-[150px]">
                         <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-white">{footerLink.title}</h4>
-                        <ul>
+                        <ul className="list-none mt-4">
                             {footerLink.links.map((link, index) => (
-                                <li key={link.map} className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer`}>
+                                <li key={link.map} className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer ${index !== footerLink.links.length - 1 ? "mb-4" : "mb-0"}`}>
                                     {link.name}
                                 </li>
                             ))}
