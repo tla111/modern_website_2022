e79fa2e5d66eb9d1c47a5e6535433c81b5f558fd
Tim La <tlamarketing111@gmail.com>
2022-08-23 07:12:30 -0400

    dimmed out gradient intensity

--- bank_modern_app/src/components/Hero.jsx
+++ bank_modern_app/src/components/Hero.jsx
@@ -34,7 +34,7 @@ const Hero = () => {
                 </p>
             </div>
 
-            <div>
+            <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
                 <img src={robot} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
                 <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient"></div>
                 <div className="absolute z-[1] w-[80%] h-[80%] rounded-full bottom-40 white__gradient"></div>
