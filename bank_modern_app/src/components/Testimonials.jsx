ff82e0c44a9334c00d0769322d72cf3b9d97bfdc
Tim La <tlamarketing111@gmail.com>
2022-08-23 11:08:43 -0400

    creating & styling testimonials component

--- bank_modern_app/src/components/Testimonials.jsx
+++ bank_modern_app/src/components/Testimonials.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { feedback } from '../constants'
+import styles from '../style'
+import FeedbackCard from './FeedbackCard'
 
-const Testimonials = () => {
-    return (
-        <div>Testimonials</div>
-    )
-}
+const Testimonials = () => (
+    <section id="client" className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
+        {/* TODO */}
+        <div />
+
+        <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
+            <h1 className={styles.heading2}>What People are <br className="sm:block hidden" /> saying about us</h1>
+            <div className="w-full md:mt-0 mt-6">
+                <p> Everything you need to accept card payments and grow your business
+                    anywhere on the planet.
+                </p>
+            </div>
+        </div>
+    </section>
+)
 
 export default Testimonials
