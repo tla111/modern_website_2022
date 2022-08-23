d561d7038638e5f6ba66c621c6c010bb8cf5983e
Tim La <tlamarketing111@gmail.com>
2022-08-23 11:57:38 -0400

    added & styled copyright

--- bank_modern_app/src/components/Footer.jsx
+++ bank_modern_app/src/components/Footer.jsx
@@ -6,6 +6,7 @@ import { footerLinks, socialMedia } from '../constants'
 const Footer = () => (
     <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
         <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
+
             <div className="flex-1 flex flex-col justify-start mr-10">
                 <img src={logo} alt="hoobank" className="w-[266px] h-[72px] object-contain" />
                 <p className={`${styles.paragraph} mt-4 max-w-[310px]`}>A new way to make the payments easy, reliable and secure.</p>
@@ -26,6 +27,11 @@ const Footer = () => (
                 ))}
             </div>
 
+        </div>
+
+        <div>
+            <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">Copyright Ⓒ 2022 HooBank. All Rights Reserved.</p>
+            <div></div>
         </div>
     </section>
 )
