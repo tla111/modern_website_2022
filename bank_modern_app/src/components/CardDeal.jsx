8b807e21247ab1cc7df2d03f81f002dbe47d6110
Tim La <tlamarketing111@gmail.com>
2022-08-23 10:57:07 -0400

    added & styled button

--- bank_modern_app/src/components/CardDeal.jsx
+++ bank_modern_app/src/components/CardDeal.jsx
@@ -10,7 +10,9 @@ const CardDeal = () => (
             <p className={`${styles.paragraph} max-w-[470px] mt-5`}> Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis
                 aliquet eget mauris tortor.ç Aliquet ultrices ac, ametau.
             </p>
+            <Button styles="mt-10" />
         </div>
+
     </section>
 )
 
