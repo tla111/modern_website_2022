519244c7697cc96b518d92affc8e410541027407
Tim La <tlamarketing111@gmail.com>
2022-08-23 10:54:29 -0400

    creating logic for cardDeal component

--- bank_modern_app/src/components/CardDeal.jsx
+++ bank_modern_app/src/components/CardDeal.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { card } from '../assets'
+import styles, { layout } from '../style'
+import Button from './Button'
 
-const CardDeal = () => {
-    return (
-        <div>CardDeal</div>
-    )
-}
+const CardDeal = () => (
+    <section className={layout.section}>
+        <div className={layout.sectionInfo}>
+            <h2>Find a better card deal <br className="sm:block hidden" /> in a few easy steps</h2>
+            <p> Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis
+                aliquet eget mauris tortor.ç Aliquet ultrices ac, ametau.
+            </p>
+        </div>
+    </section>
+)
 
 export default CardDeal
