c3ae26bd9da0df384985d0c78007283ada954709
Tim La <tlamarketing111@gmail.com>
2022-08-23 11:15:30 -0400

    styling feedback card

--- bank_modern_app/src/components/FeedbackCard.jsx
+++ bank_modern_app/src/components/FeedbackCard.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const FeedbackCard = () => {
-    return (
-        <div>FeedbackCard</div>
-    )
-}
+const FeedbackCard = ({ content, name, title, img }) => (
+    <div className="flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
+
+    </div>
+)
 
 export default FeedbackCard
