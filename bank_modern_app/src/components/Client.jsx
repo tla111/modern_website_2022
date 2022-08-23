43ebd62c10644a1a9c8ca236b2597b55ed4df35d
Tim La <tlamarketing111@gmail.com>
2022-08-23 12:03:49 -0400

    styled social media links & added section id for client component

--- bank_modern_app/src/components/Client.jsx
+++ bank_modern_app/src/components/Client.jsx
@@ -3,7 +3,7 @@ import { clients } from '../constants'
 import styles from '../style'
 
 const Client = () => (
-    <section className={`${styles.flexCenter} my-4`}>
+    <section id="clients" className={`${styles.flexCenter} my-4`}>
         <div className={`${styles.flexCenter} flex-wrap w-full`}>
             {clients.map((client) => (
                 <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}>
